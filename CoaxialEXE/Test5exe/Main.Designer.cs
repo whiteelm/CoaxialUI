a7ec3ae1063478fbe6a5940cd98a2e7b676d90ce
thiswhitenike@gmail.com <thiswhitenike@gmail.com>
2022-07-28 09:18:58 +0700

    Создано решение

---
+++ CoaxialEXE/Test5exe/Main.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Test5exe
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.ZoLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.DLabel = new System.Windows.Forms.Label();
+            this.dLab = new System.Windows.Forms.Label();
+            this.ErLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.Ereflabel = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(168, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 26);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "=>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(244, 67);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(14, 13);
+            this.label10.TabIndex = 49;
+            this.label10.Text = "C";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(245, 95);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 50;
+            this.label11.Text = "L";
+            // 
+            // ZoLabel
+            // 
+            this.ZoLabel.AutoSize = true;
+            this.ZoLabel.Location = new System.Drawing.Point(238, 14);
+            this.ZoLabel.Name = "ZoLabel";
+            this.ZoLabel.Size = new System.Drawing.Size(20, 13);
+            this.ZoLabel.TabIndex = 85;
+            this.ZoLabel.Text = "Zo";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(29, 11);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(96, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(29, 38);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(96, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "0,1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(29, 65);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(96, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "1";
+            // 
+            // DLabel
+            // 
+            this.DLabel.AutoSize = true;
+            this.DLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DLabel.Location = new System.Drawing.Point(8, 14);
+            this.DLabel.Name = "DLabel";
+            this.DLabel.Size = new System.Drawing.Size(15, 13);
+            this.DLabel.TabIndex = 10;
+            this.DLabel.Text = "D";
+            // 
+            // dLab
+            // 
+            this.dLab.AutoSize = true;
+            this.dLab.Location = new System.Drawing.Point(8, 41);
+            this.dLab.Name = "dLab";
+            this.dLab.Size = new System.Drawing.Size(13, 13);
+            this.dLab.TabIndex = 11;
+            this.dLab.Text = "d";
+            // 
+            // ErLabel
+            // 
+            this.ErLabel.AutoSize = true;
+            this.ErLabel.Location = new System.Drawing.Point(6, 68);
+            this.ErLabel.Name = "ErLabel";
+            this.ErLabel.Size = new System.Drawing.Size(17, 13);
+            this.ErLabel.TabIndex = 12;
+            this.ErLabel.Text = "Er";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(366, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 87;
+            this.label1.Text = "pF/m";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(366, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 88;
+            this.label5.Text = "uH/m";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(366, 14);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 89;
+            this.label6.Text = "Ohm";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(131, 14);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 90;
+            this.label7.Text = "mm";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(131, 41);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 13);
+            this.label8.TabIndex = 91;
+            this.label8.Text = "mm";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(264, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 20);
+            this.textBox1.TabIndex = 92;
+            this.textBox1.Text = "1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox5.Location = new System.Drawing.Point(264, 65);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(96, 20);
+            this.textBox5.TabIndex = 96;
+            this.textBox5.Text = "1";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox6.Location = new System.Drawing.Point(264, 92);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(96, 20);
+            this.textBox6.TabIndex = 97;
+            this.textBox6.Text = "1";
+            // 
+            // Ereflabel
+            // 
+            this.Ereflabel.AutoSize = true;
+            this.Ereflabel.Location = new System.Drawing.Point(232, 41);
+            this.Ereflabel.Name = "Ereflabel";
+            this.Ereflabel.Size = new System.Drawing.Size(26, 13);
+            this.Ereflabel.TabIndex = 99;
+            this.Ereflabel.Text = "Eref";
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox7.Location = new System.Drawing.Point(264, 38);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(96, 20);
+            this.textBox7.TabIndex = 98;
+            this.textBox7.Text = "1";
+            // 
+            // Button2
+            // 
+            this.Button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Button2.Location = new System.Drawing.Point(168, 47);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(48, 26);
+            this.Button2.TabIndex = 100;
+            this.Button2.Text = "<=";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(496, 166);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Ereflabel);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ZoLabel);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ErLabel);
+            this.Controls.Add(this.dLab);
+            this.Controls.Add(this.DLabel);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Coaxial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label ZoLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label DLabel;
+        private System.Windows.Forms.Label dLab;
+        private System.Windows.Forms.Label ErLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label Ereflabel;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button Button2;
+    }
+}
+
---
+++ CoaxialEXE/Test5exe/Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Test5exe
+{
+    public partial class Main : Form
+    {
+        [DllImport(@"Coaxial.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl, EntryPoint = "COAXIAL")]
+        private static extern void Coaxial(ref double er, ref double a, ref double b, ref double dC,
+            ref double dL, ref double dZ0, int mod);
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Calculate(1);
+            ZoLabel.Font = new Font(ZoLabel.Font, FontStyle.Underline);
+            dLab.Font = new Font(dLab.Font, FontStyle.Regular);
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Calculate(2);
+            dLab.Font = new Font(dLab.Font, FontStyle.Underline);
+            ZoLabel.Font = new Font(ZoLabel.Font, FontStyle.Regular);
+        }
+
+        private void Calculate(int mod)
+        {
+            double dC = 0;
+            double dL = 0;
+            try
+            {
+                var dZ0 = Converter(textBox1);
+                var b = Converter(textBox2);
+                var a = Converter(textBox3);
+                var er = Converter(textBox4);
+                
+                Coaxial(ref er, ref a, ref b, ref dC, ref dL, ref dZ0, mod);
+
+                textBox1.Text = NumberRound(dZ0);
+                textBox2.Text = NumberRound(b);
+                textBox3.Text = NumberRound(a);
+                textBox4.Text = NumberRound(er);
+                textBox5.Text = NumberRound(dC);
+                textBox6.Text = NumberRound(dL);
+                textBox7.Text = NumberRound(er);
+            }
+            catch
+            {
+                MessageBox.Show(@"Ошибка в заполнении", @"Внимание!", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static double Converter(Control textBox)
+        {
+            var replace = textBox.Text.Replace(".", ",");
+            return Convert.ToDouble(replace);
+        }
+
+        private static string NumberRound(double num)
+        {
+            return Math.Round(num, 4).ToString(CultureInfo.CurrentCulture);
+        }
+    }
+}
